3c56b5862531c608c5a9cbb38fa81d2776313f6a
Nikhil Nitin Ranjan <72209132+nikhilnitinranjan@users.noreply.github.com>
2022-02-03 16:01:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+'use strict';
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displaMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+}
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    //When there is no input!
+    displaMessage('No Number!');
+
+  } else if (guess === secretNumber) {
+    //When player wins!
+    displaMessage('Correct Number');
+    document.querySelector('.number').textContent = secretNumber;
+
+    //backgroundColor change when guess is right
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {  
+    displaMessage( guess > secretNumber ? 'Too High!' : 'Too Low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+        displaMessage('You Lost The Game');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  
+  displaMessage('Start guessing...')
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
